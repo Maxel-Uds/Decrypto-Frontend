1742d59095313c4d23f696bc985dd98bd4cca8b1
Maxel-Uds <maxellopes32@gmail.com>
2022-06-30 15:02:26 -0300

    Header

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -3,8 +3,9 @@ import './Header.css';
 
 export default function header(props) {
     return (
-        <header className="header">
-            Header
+        <header className="header d-none d-sm-flex flex-column">
+            <h1><i className="fa fa-home">Início</i></h1>
+            <p className="lead text-muted">Bem vindo a Decrypto! Codifique e decodifique mensagens por segurança ou diversão</p>
         </header>
     );
 }
