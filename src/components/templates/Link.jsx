1e7dcee1b113f07f44f84d197f99f957d5dd53c2
Maxel-Uds <maxellopes32@gmail.com>
2022-06-30 12:33:46 -0300

    Templates criados

---
+++ src/components/templates/Link.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Link.css';
+
+export default function link(props) {
+    return (
+        <a href="/" className="link">
+            Link
+        </a>
+    );
+}
