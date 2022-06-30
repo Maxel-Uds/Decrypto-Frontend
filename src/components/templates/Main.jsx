a69583db68aa1b826e504acace2a9b1dda8bef32
Maxel-Uds <maxellopes32@gmail.com>
2022-06-30 18:34:44 -0300

    Componente conteúdo

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -6,8 +6,12 @@ export default function main(props) {
     return (
         <Fragment>
             <Header/>
-            <main className="content">
-                Conteúdo
+            <main className="content container-fluid">
+                <div className="p-3 mt-3">
+                    <h1 className="display-4">Bem Vindo ao Decrypto!</h1>
+                    <hr/>
+                    <p className="lead mb-0">Aqui você encontrará um meio seguro e eficiente de criptografar suas mensagens com um simples clique!</p>
+                </div>
             </main>
         </Fragment>
     );
