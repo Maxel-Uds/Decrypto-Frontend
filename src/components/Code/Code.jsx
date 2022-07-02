607889420a2c444795f706f5ade4ba2aec08e9c6
Maxel-Uds <maxellopes32@gmail.com>
2022-07-01 21:46:11 -0300

    Implementando validação no formulário do componente Code

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -9,7 +9,8 @@ const initialState = {
         message: ''
     },
     messageId: '',
-    codeMessage: ''
+    codeMessage: '',
+    ableButton: false
 }
 
 export default class Code extends Component {
@@ -19,19 +20,20 @@ export default class Code extends Component {
     constructor(props) {
         super(props);
 
-        this.updateField = this.updateField.bind(this);
         this.clearFields = this.clearFields.bind(this);
+        this.updateField = this.updateField.bind(this);
         this.handleOnChange = this.handleOnChange.bind(this);
     }
 
     renderForm() {
         return (
-            <div className="form">
+            <form className="was-validated">
                 <div className="row">
                     <div className="form-group row">
                         <label className="col-12 col-6  col-md-3 col-form-label">Senha:</label>
                         <div className="col-12 col-7 col-md-9">
-                            <input type="password" className="form-control" name="pass" placeholder="Digite a senha..." value={this.state.editable.pass} onChange={this.handleOnChange}/>
+                            <input type="password" className="form-control" name="pass" required minLength="5" placeholder="Digite a senha..." value={this.state.editable.pass} onChange={this.handleOnChange}/>
+                            <div className="invalid-feedback">Digite uma senha de no mínimo 5 caracteres!</div>
                         </div>
                     </div>
                 </div>
@@ -39,8 +41,9 @@ export default class Code extends Component {
                 <div className="row">
                     <div className="form-group row">
                         <label className="col-12 col-md-3 col-form-label">Confirme a Senha:</label>
-                        <div className=" col-12 col-md-9">
-                            <input type="password" className="form-control" name="passConfirm" placeholder="Senha novamente..." value={this.state.editable.passConfirm} onChange={this.handleOnChange}/>
+                        <div className=" col-12 col-md-9" id="confirmPassDiv">
+                            <input type="password" className="form-control" name="passConfirm" required id="passConfirmInput" placeholder="Senha novamente..." value={this.state.editable.passConfirm} onChange={this.handleOnChange}/>
+                            <div className="invalid-feedback" id="warningDiv">As senhas precisam ser iguais!</div>
                         </div>
                     </div>
                 </div>
@@ -49,7 +52,8 @@ export default class Code extends Component {
                     <div className="form-group row">
                         <label className="col-12 col-md-3 col-form-label">Mensagem:</label>
                         <div className="col-12 col-md-9">
-                            <textarea type="text" className="form-control" name="message" placeholder="Digite a mensagem..." value={this.state.editable.message} onChange={this.updateField}></textarea>
+                            <textarea type="text" className="form-control" name="message" required minLength="20" placeholder="Digite a mensagem..." value={this.state.editable.message} onChange={this.updateField}></textarea>
+                            <div className="invalid-feedback">Digite uma mensagem com no mínimo 20 caracteres!</div>
                         </div>
                     </div>
                 </div>
@@ -75,12 +79,12 @@ export default class Code extends Component {
                 <hr/>
 
                 <div className="row">
-                   <div className="col-12 d-flex justify-content-center">
-                       <button className="btn btn-success">Codificar</button>
-                       <button className="btn btn-danger mx-2" onClick={this.clearFields}>Cancelar</button>
-                   </div>
-                </div>
-            </div>
+                    <div className="col-12 d-flex justify-content-center">
+                        <button className="btn btn-success">Codificar</button>
+                        <button className="btn btn-danger mx-2" onClick={this.clearFields}>Cancelar</button>
+                    </div>
+               </div>
+            </form>
         );
     }
 
@@ -91,8 +95,7 @@ export default class Code extends Component {
     handleOnChange(event) {
         this.updateField(event);
 
-        setTimeout(() => this.checkPassIsValid(event), 50);
-        
+        setTimeout(() => this.confirmPass(), 10);
     }
 
     updateField(event) {
@@ -101,16 +104,9 @@ export default class Code extends Component {
         this.setState({ editable });
     }
 
-    checkPassIsValid(event) {
-        const pass = this.state.editable.pass;
-        const passConfirm = this.state.editable.passConfirm;
-        
-        if(pass !== passConfirm) {
-            console.log(pass, passConfirm);
-        }
-        else {
-            console.log("igual");
-        }
+    confirmPass() {
+        const passConfirmInput = document.getElementById("passConfirmInput");
+        return (this.state.editable.pass !== this.state.editable.passConfirm) ? passConfirmInput.classList.add("is-invalid") : passConfirmInput.classList.remove("is-invalid");
     }
 
     render() {
