c0f566da02c575581180b014bb074c3eb502e61c
Maxel-Uds <maxellopes32@gmail.com>
2022-07-04 11:07:08 -0300

    Mudando o label do botão

--- src/components/Forms/DecodeForm.jsx
+++ src/components/Forms/DecodeForm.jsx
@@ -47,7 +47,7 @@ export default function form(props) {
 
             <div className="row">
                 <div className="col-12 d-flex justify-content-center">
-                    <button className="btn btn-success" disabled={props.disabled}>Codificar</button>
+                    <button className="btn btn-success" disabled={props.disabled}>Decodificar</button>
                     <button className="btn btn-danger mx-2" onClick={() => props.clear()}>Cancelar</button>
                 </div>
             </div>
