afe4fdf1b4651750ba716df44d60c8ba8e71b8de
Maxel-Uds <maxellopes32@gmail.com>
2022-07-07 13:45:00 -0300

    Fazendo a plicação ser renderizada somente após o backend subir

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Axios from 'axios';
 import App from './main/App';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+
+async function upApplication() {
+  await Axios.get('https://the-encryptor.herokuapp.com/encrypt/up')
+    .then(resp => {
+    console.log(resp.data);
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<React.StrictMode><App/></React.StrictMode>);
+  })
+  .catch(err => {
+    alert('Algum erro ocorreu e não conseguimos acessar a aplicação!');
+  });
+}
+
+upApplication();
